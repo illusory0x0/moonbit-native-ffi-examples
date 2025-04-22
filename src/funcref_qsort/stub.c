fac4fa3cf687f970eb7e8b43fcccc38b0fa40c90
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-04-22 11:58:43 +0800

    use moon run to test

---
+++ src/funcref_qsort/stub.c
@@ -0,0 +1,49 @@
+#include <moonbit.h>
+#define __USE_GNU // to order to use qsort_r
+#include <stdlib.h>
+
+typedef void *moonbit_ref_t;
+typedef moonbit_ref_t moonbit_point_t;
+typedef moonbit_point_t *moonbit_fixedarray_point_t;
+
+struct comp_context {
+  int (*call)(moonbit_point_t lhs, moonbit_point_t rhs);
+};
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__)
+#include <stdlib.h>
+int comp(struct comp_context *context, moonbit_point_t *lhs,
+         moonbit_point_t *rhs)
+#else
+#define __USE_GNU // to order to use qsort_r
+#include <stdlib.h>
+int comp(moonbit_point_t *lhs, moonbit_point_t *rhs,
+         struct comp_context *context)
+#endif
+{
+  moonbit_incref(*lhs);
+  moonbit_incref(*rhs);
+  int res = context->call(*lhs, *rhs);
+  return res;
+}
+
+// https://en.cppreference.com/w/c/algorithm/qsort
+// both glibc and Windows CRT doesn't support ISO C qsort_s
+void ffi_qsort(moonbit_fixedarray_point_t xs,
+               int (*call)(moonbit_point_t lhs, moonbit_point_t rhs)) {
+  size_t count = Moonbit_array_length(xs);
+  size_t elem_size = sizeof(moonbit_point_t);
+
+  struct comp_context context;
+  context.call = call;
+#if defined(_WIN32) || defined(_WIN64)
+  // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/qsort-s?view=msvc-170
+  qsort_s(xs, count, elem_size, (void *)comp, &context);
+#elif defined(__APPLE__)
+  // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/qsort_r.3.html
+  qsort_r(xs, count, elem_size, &context, (void *)comp);
+#else
+  // https://www.man7.org/linux/man-pages/man3/qsort.3.html
+  qsort_r(xs, count, elem_size, (void *)comp, &context);
+#endif
+}
