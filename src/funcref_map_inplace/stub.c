1640e1c3d1330eae10275cabe68ab8c742ccbc46
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-04-23 13:43:54 +0800

    save

--- src/funcref_map_inplace/stub.c
+++ src/funcref_map_inplace/stub.c
@@ -2,21 +2,6 @@
 
 typedef void *moonbit_closure_t;
 
-// `call` indicates how to consume `closure`
-void register_callback(void (*call)(moonbit_closure_t),
-                       moonbit_closure_t closure) {
-  moonbit_incref(closure);
-  // call closure before must insert `moonbit_incref`
-  // https://github.com/moonbitlang/moonbit-docs/issues/664
-  call(closure); // call closure directly
-
-  moonbit_incref(closure);
-  call(closure);
-
-  moonbit_incref(closure);
-  call(closure);
-}
-
 typedef void *moonbit_point_t;
 
 void ffi_map_inplace(moonbit_point_t *xs,
