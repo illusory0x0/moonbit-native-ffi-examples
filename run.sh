f610a6a5600af92666467a78f7c8006493da2b4c
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-04-23 13:03:47 +0800

    save

--- run.sh
+++
@@ -1,5 +0,0 @@
-moon run --target native src/funcref_qsort/ > snapshot.txt
-moon run --target native src/funcref_qsort_without_stub/ >> snapshot.txt
-moon run --target native src/funcref_qsort_closure/ >> snapshot.txt     
-moon run --target native src/funcref_register_callback/ >> snapshot.txt
-moon run --target native src/funcref_map_inplace/ >> snapshot.txt
--- src/funcref_map_inplace/stub.c
+++ src/funcref_map_inplace/stub.c
@@ -1,29 +1,36 @@
 #include <moonbit.h>
 
-typedef void* moonbit_closure_t;
+typedef void *moonbit_closure_t;
 
 // `call` indicates how to consume `closure`
-void register_callback(void (*call)(moonbit_closure_t), moonbit_closure_t closure) {
-  moonbit_incref(closure); 
+void register_callback(void (*call)(moonbit_closure_t),
+                       moonbit_closure_t closure) {
+  moonbit_incref(closure);
   // call closure before must insert `moonbit_incref`
   // https://github.com/moonbitlang/moonbit-docs/issues/664
   call(closure); // call closure directly
-  
-  moonbit_incref(closure);
-  call(closure);
 
   moonbit_incref(closure);
   call(closure);
-} 
 
-typedef void* moonbit_point_t ;
+  moonbit_incref(closure);
+  call(closure);
+}
 
-void ffi_map_inplace(moonbit_point_t* xs,moonbit_point_t (*call)(moonbit_closure_t,moonbit_point_t), moonbit_closure_t closure) {
+typedef void *moonbit_point_t;
+
+void ffi_map_inplace(moonbit_point_t *xs,
+                     moonbit_point_t (*call)(moonbit_closure_t,
+                                             moonbit_point_t),
+                     moonbit_closure_t closure) {
   int len = Moonbit_array_length(xs);
 
-  for (int i = 0 ; i < len; ++i) {
-    // moonbit_incref(xs[i]);
+  for (int i = 0; i < len; ++i) {
     moonbit_incref(closure);
-    xs[i] = call(closure,xs[i]);
+
+    xs[i] = call(closure, xs[i]);
+    
+    moonbit_incref(closure);
+    xs[i] = call(closure, xs[i]);
   }
 }
